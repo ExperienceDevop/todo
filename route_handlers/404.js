6c7f197ed670c1d823d989afba3bd68ab1eb67a0
ExperienceDevop <109736678+ExperienceDevop@users.noreply.github.com>
2024-11-05 01:25:02 +0000

    Fixed

---
+++ route_handlers/404.js
@@ -0,0 +1,23 @@
+const Handler = (props)=>
+    {
+      for (let x in props.data.methods)
+      {
+        if (props.data.methods[x] == "GET")
+        {
+          this.get = get ({express: props.express, data: props.data})
+        }
+      }
+    
+      return this
+    }
+    
+    const get = (props)=>
+    {
+      props.express.get (props.data.path, (request, response)=>{
+        response.status (props.data.status)
+        response.append('Content-Type', `${props.data.content_type}; charset=UTF-8`)
+        response.send ('Page does not exist...')
+      })
+    }
+    
+    module.exports = Handler
